6f3958feafbe06a6926ab8ca453df7d3375f97e1
Daniel Laufenberg <danlanfen26@gmail.com>
2020-03-26 19:20:29 +0000

    client origin added

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/myusedcarsalesman',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/myusedcarsalesman_test',
-    CLIENT_ORIGIN: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
+    CLIENT_ORIGIN: process.env.REACT_APP_BASE_URL || 'https://myusedcarsalesman-client.now.sh',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
   }
