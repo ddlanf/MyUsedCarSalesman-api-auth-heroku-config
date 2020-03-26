17eaa2f7983601747966f8a7152ba4b1d5ed26f9
Daniel Laufenberg <danlanfen26@gmail.com>
2020-03-26 18:50:05 +0000

    database url added

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/blogful-auth',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/myusedcarsalesman',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/myusedcarsalesman_test',
+    CLIENT_ORIGIN: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
   }
