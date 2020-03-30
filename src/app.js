c847f0c74ebe940c9c6b54707b120c9f2429db9e
Daniel Laufenberg <danlanfen26@gmail.com>
2020-03-30 21:28:40 +0000

    client url changed

--- src/app.js
+++ src/app.js
@@ -19,12 +19,15 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-
+/*
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 )
+*/
+
+app.use(cors())
 
 app.use('/api/users', usersRouter)
 app.use('/api/posts', postsRouter)
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/myusedcarsalesman',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/myusedcarsalesman_test',
-    CLIENT_ORIGIN: process.env.REACT_APP_BASE_URL || 'https://myusedcarsalesman-client.now.sh',
+    CLIENT_ORIGIN: process.env.REACT_APP_BASE_URL || 'https://myusedcarsalesman.now.sh',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
   }
